f34316f2b17bf2048aced3788d5670dbd2242e8a
hernah <hgalosada@gmail.com>
2024-10-05 06:42:10 +0800

    Add in view to edit student information

--- OjtPortal/Pages/ChangeDefaultPassword.cshtml.cs
+++ OjtPortal/Pages/ChangeDefaultPassword.cshtml.cs
@@ -1,46 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using OjtPortal.Dtos;
+using OjtPortal.Entities;
+using OjtPortal.Infrastructure;
+using OjtPortal.Repositories;
 using OjtPortal.Services;
+using System.Text.Json;
 
 namespace OjtPortal.Pages
 {
     public class ChangeDefaultPassword : PageModel
     {
-		private readonly IUserService _userService;
+        private readonly IUserService _userService;
+        private readonly IDepartmentRepo _departmentRepo;
+        private readonly IDegreeProgramRepo _degreeProgramRepo;
+        private readonly ITeacherRepo _teacherRepo;
 
-		[BindProperty(SupportsGet = true)]
+        // Properties bound to the form fields
+        [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
         [BindProperty(SupportsGet = true)]
-        public string Token { get; set; }
+        public string Token { get; set; } = string.Empty;
+        [BindProperty(SupportsGet = true)]
+        public bool PendingStudentUpdate { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public bool PendingEmailUpdate { get; set; }
+
+        [BindProperty]
+        public string StudentId { get; set; } = string.Empty;
+        [BindProperty]
+        public string Email { get; set; } = string.Empty;
+        [BindProperty]
+        public string FirstName { get; set; } = string.Empty;
+        [BindProperty]
+        public string LastName { get; set; } = string.Empty;
+
+        [BindProperty]
+        public int? Department { get; set; } = null;
+        [BindProperty]
+        public int? DegreeProgram { get; set; } = null;
+        [BindProperty]
+        public int? Instructor { get; set; } = null;
+
         [BindProperty]
         public string NewPassword { get; set; } = string.Empty;
         [BindProperty]
         public string ConfirmPassword { get; set; } = string.Empty;
 
-		public ChangeDefaultPassword(IUserService userService)
-		{
-			this._userService = userService;
-		}
+        public List<Department> Departments { get; set; } = new();
+        public List<DegreeProgram> DegreePrograms { get; set; } = new();
+        public List<Teacher>? Teachers { get; set; } = new();
 
-        public void OnGet()
+        public ChangeDefaultPassword(
+            IUserService userService,
+            IDepartmentRepo departmentRepo,
+            IDegreeProgramRepo degreeProgramRepo,
+            ITeacherRepo teacherRepo)
         {
-            // Clear TempData when the page is accessed via GET
+            _userService = userService;
+            _departmentRepo = departmentRepo;
+            _degreeProgramRepo = degreeProgramRepo;
+            _teacherRepo = teacherRepo;
+        }
+
+        public async Task OnGetAsync()
+        {
+            Departments = await _departmentRepo.GetDepartmentsAsync();
+            DegreePrograms = await _degreeProgramRepo.GetDegreeProgramsAsync();
+            Teachers = await _teacherRepo.GetTeachersAsync();
+
             TempData.Remove("ErrorMessage");
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            TempData.Remove("ErrorMessage");
-            if (string.IsNullOrEmpty(NewPassword) || string.IsNullOrEmpty(ConfirmPassword))
+            Departments = await _departmentRepo.GetDepartmentsAsync();
+            DegreePrograms = await _degreeProgramRepo.GetDegreeProgramsAsync();
+            Teachers = await _teacherRepo.GetTeachersAsync();
+
+            if (Email != null && !EmailChecker.IsEmailValid(Email))
             {
-                TempData["ErrorMessage"] = "Password must not be empty";
+                TempData["ErrorMessage"] = "Please use your institutional email";
+                ModelState.AddModelError(string.Empty, "Invalid email format.");
                 return Page();
             }
+
             if (!NewPassword.Equals(ConfirmPassword))
             {
                 TempData["ErrorMessage"] = "Passwords do not match";
+                ModelState.AddModelError(string.Empty, "Passwords do not match.");
                 return Page();
             }
 
@@ -53,12 +101,27 @@ namespace OjtPortal.Pages
 
             var (message, error) = await _userService.ChangeDefaultPasswordAsync(changePasswordDto, Token);
 
-            if(error != null)
+            if (error != null)
             {
                 TempData["Message"] = error.Errors.First().Message;
+                return Page();
             }
 
             return RedirectToPage("RedirectChangeDefaultPassword");
+        }
+
+        public async Task<IActionResult> OnPostDepartmentSelectAsync()
+        {
+            DegreePrograms = await _degreeProgramRepo.GetDegreeProgramsAsync();
+            DegreePrograms = DegreePrograms.Where(dp => dp.DepartmentId == Department).ToList();
+            Teachers = await _teacherRepo.GetTeachersAsync();
+            Teachers = Teachers!.Where(t => t.Department.DepartmentId == Department).ToList();
+
+            return new JsonResult(new
+            {
+                DegreePrograms,
+                Teachers
+            });
         }
     }
 }
